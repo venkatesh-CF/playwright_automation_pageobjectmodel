17ab78beb88932d68340c91659edc0c5f1f6d184
Venkatesh <venkyposhanapelli@gmail.com>
2026-07-09 08:04:12 +0530

    Set Playwright to run in headless mode

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://practicesoftwaretesting.com/',
-    headless: false,
+    headless: true,
     viewport: null,
     slowMo: 500,
     launchOptions: {
