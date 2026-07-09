3d0355af2d45eeca1cbe9c2f816afb064043fd64
Venkatesh <venkyposhanapelli@gmail.com>
2026-07-09 08:48:01 +0530

    Reduce retry attempts in CI from 2 to 1 for improved test efficiency

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,13 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://practicesoftwaretesting.com/',
-    headless: true,
-    viewport: null,
-    slowMo: 500,
-    launchOptions: {
-      args: ['--start-maximized'],
-    },
-
+    headless: true,   
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
